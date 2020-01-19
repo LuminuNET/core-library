3a63d7887f796da3a8ccbc5c91d8c66ede90de2a
Keimeno <constantin@metzworld.com>
2020-01-19 23:26:20 +0100

    added exports to base root

---
+++ index.ts
@@ -0,0 +1,6 @@
+export { default as redis } from './common/redis.service';
+export { default as buffer } from './common/buffer.service';
+
+export * from './common/redis.service';
+export * from './common/buffer.service';
+export * from './common/locale.service';

02af6a55d79f17cb6677adfb702397da53c07244
Keimeno <constantin@metzworld.com>
2020-01-20 19:06:46 +0100

    deleted

--- common/locale.service.ts
+++
@@ -1,9 +0,0 @@
-const LOCALE_KEY: string = 'lm:locale';
-
-export const toggleLocale = (): void => {
-	window.localStorage.setItem(LOCALE_KEY, getLocale() === 'en' ? 'de' : 'en');
-};
-
-export const getLocale = (): string | null => {
-	return window.localStorage.getItem(LOCALE_KEY);
-};
--- index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	...require('./common/redis.service'),
-	...require('./common/buffer.service'),
-	...require('./common/locale.service')
-};

1c573f2cb90a2be712238f8d4be579dea961712f
Keimeno <constantin@metzworld.com>
2020-01-20 18:49:52 +0100

    moved everything to source and now serving index file that manages exports

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as Redis from './common/redis.service';
+import * as Locale from './common/locale.service';
+import Buffer from './common/buffer.service';
+
+export { Redis, Buffer, Locale };

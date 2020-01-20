fba5266941851adae2d8008ff690b3c335218353
Keimeno <constantin@metzworld.com>
2020-01-20 18:49:35 +0100

    added typings

---
+++ types/index.d.ts
@@ -0,0 +1,7 @@
+import * as Redis from '../src/common/redis.service';
+import * as Locale from '../src/common/locale.service';
+import Buffer from '../src/common/buffer.service';
+
+export { Redis, Locale, Buffer };
+
+declare module '@luminu/core';
